fb320f95228a898d345836a2f1345ff9811acdfa
shunsuke-tamura <wwwxx4869@gmail.com>
2021-09-05 23:04:46 +0900

    vuetifyを導入

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-    configureWebpack: {
-      devServer: {
-        watchOptions: {
-          poll: true
-        }
+  configureWebpack: {
+    devServer: {
+      watchOptions: {
+        poll: true
       }
     }
-  }
+  },
+
+  transpileDependencies: [
+    'vuetify'
+  ]
+}
   

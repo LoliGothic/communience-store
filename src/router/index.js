39403b49f1950908d61c9a685341b82d58181781
saku-1101 <sakupi1101@outlook.jp>
2021-09-27 19:49:30 +0900

    ...

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Test from '../views/Test.vue'
 import Card_MY from '../views/MY-intro-card.vue'
+import Edit_page from '../views/Card-edit.vue'
 
 Vue.use(VueRouter)
 
@@ -30,6 +31,12 @@ const routes = [
     name: 'my-card',
     component: Card_MY
   },
+  {
+    path: '/edit-page',
+    name: 'edit-page',
+    component: Edit_page
+  },
+
 ]
 
 const router = new VueRouter({

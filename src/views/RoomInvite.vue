b1ae2fee81268cf6dcbf8267e92010925cc52662
LoliGothic <nana40481023@gmail.com>
2021-10-06 10:44:39 +0900

    RoomInvite.vue追加

---
+++ src/views/RoomInvite.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <RoomInvite />
+    </div>
+</template>
+
+<script>
+import RoomInvite from '@/components/Room/RoomInvite.vue'
+export default{
+    components:{
+        'RoomInvite': RoomInvite
+    }
+}
+</script>
+
+<style>
+
+</style>

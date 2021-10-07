9ec8af8824dd381966041a15da81ecf942b18092
chinami_29 <banana.work594@gmail.com>
2021-10-07 19:30:51 +0900

    ルーム一覧を作成

---
+++ src/views/RoomList.vue
@@ -0,0 +1,26 @@
+<template>
+  
+ <v-container class="grey lighten-5">
+    <v-row
+      v-for="j in 6"
+      :key="j"
+    >
+      <v-col
+        v-for="k in 2"
+        :key="k"
+        md="4"
+
+        
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          ルーム {{ k+(j-1)*2 }}
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+  
+</template>

922145648d9ce2bbd4b38fe9dcd78dfcccfdf8b8
shunsuke-tamura <wwwxx4869@gmail.com>
2021-10-09 06:00:28 +0900

    update: ヘッダーと追加ボタンの追加

--- src/views/RoomList.vue
+++ src/views/RoomList.vue
@@ -1,28 +1,39 @@
 <template>
-
- <v-container class="grey lighten-5">
-    <v-row
-      v-for="j in 6"
-      :key="j"
-    >
-      <v-col
-        v-for="k in 2"
-        :key="k"
-        md="4"
-
-        
-      >
-        
-        <v-btn
-          elevation="2"
-          class="pa-2"
-          outlined
-          tile
-        >
-          ルーム {{ k+(j-1)*2 }}
+  <v-container class="grey lighten-5">
+    <v-row>
+      <v-col md="4" offset-md="4" align-self="center">
+        <p style="font-size: 2em; text-align: center">ルーム一覧</p>
+      </v-col>
+      <v-col md="1" offset-md="3">
+        <v-btn class="mb-2" fab dark color="indigo">
+          <v-icon dark> mdi-plus </v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row v-for="j in 3" :key="j">
+      <v-col v-for="k in 2" :key="k" md="6">
+        <v-btn block elevation="2" class="pa-2" x-large outlined tile>
+          {{ RoomName[k + (j - 1) * 2 - 1] }}
         </v-btn>
       </v-col>
     </v-row>
   </v-container>
-  
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      RoomName: [
+        "理工学部",
+        "知能情報システム",
+        "SCLA",
+        "軽音部",
+        "英語",
+        "情報システム実験",
+      ]
+    }
+  }
+}
+</script>
+
